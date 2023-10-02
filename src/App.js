ea026a0b3642a0ab3867483a0b9879784b32a763
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-10-02 18:51:03 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Homepage from './pages/homepage';
-import Navbar from './components/navbar';
+import Homepage from './pages/Homepage';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 function App() {

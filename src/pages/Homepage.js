183a5fd782b0e3ddc7d57539c87be6abfd1ff6c5
Suryansh Singh <80690023+suryanshsingh2001@users.noreply.github.com>
2023-10-03 13:28:27 +0530

    Added Button.jsx

--- src/pages/Homepage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Homepage = () => {
-  return (
-    <div>Homepage</div>
-  )
-}
-
-export default Homepage
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      'primary': "#ECEEFF",
+      "btntheme": "#D400FF",
+      "slate-gray": "#6D6D6D",
+      "pale-blue": "#F5F6FF",
+      "white-400": "rgba(255, 255, 255, 0.80)"
+    }},
   },
   plugins: [],
 }

c085487c8d2d20ea2d2c417c97d949e9ee4e1910
Ravi Yadav <shortanswer12345@gmail.com>
2023-10-07 20:31:48 +0530

    Refactor the button component

--- src/pages/ButtonDoc.jsx
+++ src/pages/ButtonDoc.jsx
@@ -9,32 +9,37 @@ function ButtonDoc() {
             <Heading title={"Buttons"}/>
             <div className="flex flex-col items-center space-y-4">
                 {/* Default Button */}
-                <Button label={"Default Button"} />
+                <Button>
+                Default Button
+                </Button>
 
                 {/* Customized Button 1 */}
                 <Button
                     iconURL={arrowRight}
-                    label={"Customized Button 1"}
                     borderColor={"border-black"}
                     textColor={"text-white"}
                     backgroundColor={"bg-black"}
-                />
+                >
+                    Customized Button 1
+                </Button>
 
                 {/* Customized Button 2 */}
                 <Button
-                    label={"Customized Button 2"}
                     borderColor={"border-blue-500"}
                     textColor={"text-blue-500"}
                     backgroundColor={"bg-white"}
-                />
+                >
+                Customized Button 2
+                </Button>
 
                 {/* Customized Button 3 */}
                 <Button
-                    label={"Customized Button 3"}
                     borderColor={"border-green-500"}
                     textColor={"text-green-500"}
                     backgroundColor={"bg-white"}
-                />
+                >
+                Customized Button 3
+                </Button>
             </div>
         </>
     )

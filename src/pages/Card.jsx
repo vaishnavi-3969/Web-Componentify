ae6fe63a510f9b052f04a0c3f348e4fb69789274
Pavan-Sai-padavala <pavansaipadavala@gmail.com>
2023-10-12 12:51:48 +0530

    Bottom Navigation Componenet Added

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 import {Heading} from "../components/Heading";
+import BottomNav from "../components/Bottom Nav bar/BottomNavBar";
 export const Card = () => {
     return (
         <>
             <Heading title={"Card"}/>
+            <BottomNav backUrl="/Avatar" nextUrl="/Alert" backName="Avatar" nextName="Alert" />
         </>
     )
 }

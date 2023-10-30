1e0f3585b670850160cf958be02be024e2924373
mahendra <solankirakesh895@gmail.com>
2023-10-30 21:41:32 +0530

    #66 issue-fixed layout change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import { NavigationLeft } from "./components/NavigationLeft";
 import { Avatar } from "./pages/Avatar";
 import { Card } from "./pages/Card";
 import { Alert } from "./pages/Alert";
@@ -12,29 +9,32 @@ import ButtonDoc from "./pages/ButtonDoc";
 import {ModalPage} from "./pages/ModalPage";
 import {FooterPage} from "./pages/FooterPage";
 
+
+
+
 function App() {
+  
   return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <div className="home-container">
-          <NavigationLeft />
-          <div className="routes-container">
-            <Routes>
-              <Route path="/" element={<Homepage />} exact />
-              <Route path="/Button" element={<ButtonDoc />} />
-              <Route path="/Modal" element={<ModalPage />} />
-              <Route path="/Footer" element={<FooterPage />} />
-              <Route path="/Avatar" element={<Avatar />} />
-              <Route path="/card" element={<Card />} />
-              <Route path="/Alert" element={<Alert />} />
-              <Route path="/Badge" element={<Badge />} />
-            </Routes>
-          </div>
-        </div>
-      </BrowserRouter>
+    <BrowserRouter>
+    
+    <div className="home-container">
+    <div className="routes-container  ">
+      <Routes>
+      <Route path="/" element={<Homepage />}>
+     <Route path="Button" element={<ButtonDoc />} />
+     <Route path="Modal" element={<ModalPage />} />
+     <Route path="Footer" element={<FooterPage />} />
+     <Route path="Avatar" element={<Avatar />} />
+     <Route path="card" element={<Card />} />
+     <Route path="Alert" element={<Alert />} />
+     <Route path="Badge" element={<Badge />} />
+     </Route>
+      </Routes>
     </div>
+  </div>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,58 +1,20 @@
-import React from "react";
-import Button from "../components/Button";
-import arrowRight from "../assets/arrow-right.svg";
-import Accordion from "../components/Accordion";
-import Modal from "../components/Modal";
-import Feature6 from "../components/TopHeader";
-import Footer from "../components/Footer";
-import Login from "../components/Login";
+import React, { Fragment } from "react";
 import BottomNav from "../components/Bottom Nav bar/BottomNavBar";
-const accordionItems = [
-  { title: "Section 1", content: "Content for Section 1" },
-  { title: "Section 2", content: "Content for Section 2" },
-];
+import Sidebar from "../components/Sidebar";
+import Content from "../components/Content";
+
+
 
 const Homepage = () => {
   return (
-    <><div className="flex flex-col items-center space-y-4">
-      {/* Default Button */}
-      <Button>Default Button</Button>
+    <Fragment>
+    <main >
+    <Sidebar/>
+    <Content/>
+    <BottomNav backUrl=" " nextUrl="/Button" backName=" " nextName="Button" />
+    </main>
+    </Fragment>
 
-      {/* Customized Button 1 */}
-      <Button
-        iconURL={arrowRight}
-        borderColor={"border-black"}
-        textColor={"text-white"}
-        backgroundColor={"bg-black"}
-      >
-        Customized Button 1
-      </Button>
-
-      {/* Customized Button 2 */}
-      <Button
-        borderColor={"border-blue-500"}
-        textColor={"text-blue-500"}
-        backgroundColor={"bg-white"}
-      >
-        Customized Button 2
-      </Button>
-
-      {/* Customized Button 3 */}
-      <Button
-        borderColor={"border-green-500"}
-        textColor={"text-green-500"}
-        backgroundColor={"bg-white"}
-      >
-        Customized Button 3
-      </Button>
-
-      <Accordion items={accordionItems} />
-      <Login />
-      <Feature6 />
-      <Modal />
-      <Footer />
-    </div>
-    <BottomNav backUrl=" " nextUrl="/Button" backName=" " nextName="Button" /></>
   );
 };
 

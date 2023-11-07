cd3af3a46282e5d6d22f44ab714b0e5f67009438
Vaishnavi Kale <vaishnavi.kale3011@gmail.com>
2023-11-07 11:53:55 +0530

    Update Homepage.jsx
    Signed-off-by: Vaishnavi Kale <vaishnavi.kale3011@gmail.com>

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import Button from "../components/Button";
-import arrowRight from "../assets/arrow-right.svg";
-import Accordion from "../components/Accordion/Accordion";
-import Modal from "../components/Modal";
-import Feature6 from "../components/TopHeader";
-import Footer from "../components/Footer/Footer";
-import Login from "../components/Login/Login";
+// import Button from "../components/Button";
+// import arrowRight from "../assets/arrow-right.svg";
+// import Accordion from "../components/Accordion/Accordion";
+// import Modal from "../components/Modal";
+// import Feature6 from "../components/TopHeader";
+// import Footer from "../components/Footer/Footer";
+// import Login from "../components/Login/Login";
 import React, { Fragment } from "react";
 import BottomNav from "../components/Bottom Nav bar/BottomNavBar";
 import Sidebar from "../components/Sidebar";
